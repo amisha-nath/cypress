a9ab87a2a8997f40ef8e460bc6b8d0b818668158
Amisha Nath <amishanath23@gmail.com>
2025-09-18 12:13:12 +0530

    Add Cypress test, config, and GitHub Actions workflow

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,23 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://opensource-demo.orangehrmlive.com',
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-    },
+  
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      reportPageTitle: 'OrangeHRM Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts:false,
+  },
+    // baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    // setupNodeEvents(on, config) {
+    //   require('cypress-mochawesome-reporter/plugin')(on);
+    // },
   },
   screenshotOnRunFailure: true,
   screenshotsFolder: 'cypress/screenshots',
@@ -13,15 +26,5 @@ module.exports = defineConfig({
   videosFolder: 'cypress/videos',
   viewportWidth: 1280,
   viewportHeight: 720,
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true,
-    charts: true,
-    reportPageTitle: 'OrangeHRM Test Report',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-  },
+  
 });
