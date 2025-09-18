a9e8d761ec9eeb125dffe913575db850651fe560
Amisha Nath <amishanath23@gmail.com>
2025-09-18 23:04:32 +0530

    Add Cypress test, config, and GitHub Actions workflow

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,12 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/cw/**/*.cy.js',
     baseUrl: 'https://opensource-demo.orangehrmlive.com',
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+   
+  setupNodeEvents(on, config) {
+  require('cypress-mochawesome-reporter/plugin')(on);
     },
-    reporter: 'mochawesome',
+
+    reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
       reportDir: 'cypress/reports',
       overwrite: false,
